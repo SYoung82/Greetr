5f788a1b34d658865778a1bd92d00cd1bfb0683a
Scott Young <young.scottw@gmail.com>
2017-06-16 16:19:38 -0500

    demonstrate usage in app.js

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var g = G$('John', 'Doe');
 
 // chainable methods
 g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+  var loginGrtr = G$('John', 'Doe');
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+})
