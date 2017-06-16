88d6d263b71832177c4cf50d6e6f9fe840648dda
Scott Young <young.scottw@gmail.com>
2017-06-16 16:20:33 -0500

    comment app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// gets a new object, notice no need for 'new' keyword
 var g = G$('John', 'Doe');
 
 // chainable methods
 g.greet().setLang('es').greet(true).log();
 
+// let's use our object on the click of the login button
 $('#login').click(function() {
   var loginGrtr = G$('John', 'Doe');
   loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
-})
+});
